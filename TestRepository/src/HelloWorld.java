51b94a5ea04efe937fb46fbc5789c1a5cab4b3a8
kuntumallaradha <radhakuntumalla14@gmail.com>
2023-07-28 10:14:47 -0400

    modified helloworld

--- TestRepository/src/HelloWorld.java
+++ TestRepository/src/HelloWorld.java
@@ -7,6 +7,9 @@ public class HelloWorld {
 		System.out.println("Hello World@!!!!!@@@@");
 		System.out.println("My name is Radha....!!!");
 		System.out.println("Basic program printing...");
+		int a=10, b=10;
+		int c= a+b;
+		System.out.println("Adding a+b= "+ c);
 	
 
   }
